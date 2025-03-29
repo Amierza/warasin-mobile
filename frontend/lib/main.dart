ad2ccefd246becdffe7670dada72bd94e9584b93
Reyysusanto <rheinaldysusanto@gmail.com>
2025-03-29 20:29:09 +0700

    add profile page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:frontend/view/concultation_page.dart';
 import 'package:frontend/view/detail_concultation_page.dart';
 import 'package:frontend/view/forget_password_email_page.dart';
 import 'package:frontend/view/forget_password_pasword_page.dart';
+import 'package:frontend/view/profile_page.dart';
 import 'package:frontend/view/home_page.dart';
 import 'package:frontend/view/login_page.dart';
 import 'package:frontend/view/news_detail_page.dart';
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
         '/news': (context) => const NewsPage(),
         '/news_detail': (context) => const NewsDetailPage(),
         '/concultation_detail': (context) => const DetailConcultationPage(),
+        '/profile': (context) => const ProfilePage(),
       },
     );
   }
---
+++ frontend/lib/view/profile_page.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/shared/theme.dart';
+import 'package:frontend/widget/header.dart';
+import 'package:frontend/widget/navigation_bar.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      backgroundColor: primaryColor,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+              child: Header(),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                color: backgroundColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 30,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Riwayat',
+                      style: GoogleFonts.poppins(
+                        fontSize: 22,
+                        fontWeight: semiBold,
+                        color: primaryTextColor
+                      ),
+                    ),
+                    MenuButton(
+                      icon: Icons.history, 
+                      name: "Riwayat Konsultasi",
+                      onPressed: () {
+                        Get.toNamed('/history_concultation');
+                      },
+                    ),
+                    MenuButton(
+                      icon: Icons.people_outline, 
+                      name: "Riwayat Kesehatan"
+                    ),
+
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    Text(
+                      'Pengaturan',
+                      style: GoogleFonts.poppins(
+                        fontSize: 22,
+                        fontWeight: semiBold,
+                        color: primaryTextColor
+                      ),
+                    ),
+                    MenuButton(
+                      icon: Icons.settings, 
+                      name: "Pengaturan Pribadi"
+                    ),
+                    MenuButton(
+                      icon: Icons.note_outlined, 
+                      name: "Catatan Anda"
+                    ),
+                    MenuButton(
+                      icon: Icons.favorite_border_outlined, 
+                      name: "Daftar Favorit"
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const CustomNavigationBar(currentIndex: 4),
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  final IconData icon;
+  final String name;
+  final VoidCallback? onPressed;
+  
+  const MenuButton ({
+    super.key,
+    required this.icon,
+    required this.name,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      behavior: HitTestBehavior.opaque, // Agar area kosong juga merespon tap
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16, 
+          horizontal: 16
+        ),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: tertiaryTextColor
+            )
+          ),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon, 
+              color: primaryTextColor, 
+              size: 30
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Text(
+                name, 
+                style: TextStyle(
+                fontSize: 18,
+                fontWeight: medium,
+                color: primaryTextColor,
+              )),
+            ),
+            Icon(
+              Icons.chevron_right, 
+              color: tertiaryTextColor, 
+              size: 36
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
