447998b106d66313280e78a13b6c4e054a7bdbd9
Reyysusanto <rheinaldysusanto@gmail.com>
2025-06-14 15:00:07 +0700

    Create user motivation history

--- frontend/lib/service/motivation_service.dart
+++ frontend/lib/service/motivation_service.dart
@@ -27,8 +27,8 @@ class MotivationService {
       if (response.statusCode == 200) {
         final responseBody = jsonDecode(response.body);
         if (responseBody['status'] == true) {
-          final psychologData = AllMotivationResponse.fromJson(responseBody);
-          return psychologData;
+          final motivationData = AllMotivationResponse.fromJson(responseBody);
+          return motivationData;
         } else {
           final errorResponse = ErrorResponse.fromJson(responseBody);
           return errorResponse;
@@ -37,6 +37,54 @@ class MotivationService {
       if (response.statusCode == 200) {}
     } catch (error) {
       print('Error Fetching get all motivation : $error');
+      return null;
+    }
+  }
+
+  static Future<dynamic> createMotivationService(
+    String date,
+    int reaction,
+    String motivationid,
+  ) async {
+    final box = GetStorage();
+    final token = box.read('access_token');
+
+    if (token == null) return null;
+
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/user/create-user-motivation'),
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          "user_mot_display_date": date,
+          "user_mot_reaction": reaction,
+          "mot_id": motivationid,
+        }),
+      );
+
+      if (response.statusCode == 400) {
+        final errorResponse = ErrorResponse.fromJson(jsonDecode(response.body));
+        return errorResponse;
+      }
+
+      if (response.statusCode == 200) {
+        final responseBody = jsonDecode(response.body);
+        if (responseBody['status'] == true) {
+          final userMotivationData = CreateUserMotivationResponse.fromJson(
+            responseBody,
+          );
+          return userMotivationData;
+        } else {
+          final errorResponse = ErrorResponse.fromJson(responseBody);
+          return errorResponse;
+        }
+      }
+      if (response.statusCode == 200) {}
+    } catch (error) {
+      print('Error Fetching create user motivation : $error');
       return null;
     }
   }
